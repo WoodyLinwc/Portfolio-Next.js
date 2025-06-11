7b844b3b70fdd9948c693c8778e48029c56e7373
Woody Lin <wancheng.lin001@umb.edu>
2025-06-11 19:10:54 -0400

    Update SectionTitle.tsx

--- portfolio/src/components/SectionTitle.tsx
+++ portfolio/src/components/SectionTitle.tsx
@@ -1,4 +1,3 @@
-// src/components/SectionTitle.tsx
 interface SectionTitleProps {
     backgroundText: string;
     foregroundText: string;
@@ -14,10 +13,10 @@ export default function SectionTitle({
         <div className={`py-12 ${className}`}>
             <div className="container mx-auto px-8 lg:px-20 xl:px-32">
                 <div className="relative flex items-center justify-center mb-16">
-                    <h1 className="text-7xl lg:text-8xl font-bold text-gray-100 uppercase tracking-wider">
+                    <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-gray-100 uppercase tracking-wider text-center">
                         {backgroundText}
                     </h1>
-                    <h1 className="absolute text-3xl lg:text-4xl font-bold text-primary uppercase">
+                    <h1 className="absolute text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-primary uppercase text-center">
                         {foregroundText}
                     </h1>
                 </div>
