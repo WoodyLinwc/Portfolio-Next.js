ce0fa2430878a619f941f408a9b728e91e4ea377
Woody Lin <wancheng.lin001@umb.edu>
2025-06-27 21:23:50 -0400

    Update sitemap.ts

--- portfolio/src/app/sitemap.ts
+++ portfolio/src/app/sitemap.ts
@@ -14,7 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             url: `${baseUrl}/photography`,
             lastModified: new Date(),
             changeFrequency: "monthly",
-            priority: 0.8,
+            priority: 0.9,
         },
         {
             url: `${baseUrl}/tools`,
