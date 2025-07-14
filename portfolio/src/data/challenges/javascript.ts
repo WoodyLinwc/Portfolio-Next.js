ee6a3b78ef2d37dda9505db39d859c41aae49851
Woody Lin <wancheng.lin001@umb.edu>
2025-07-14 15:46:01 -0400

    Update javascript.ts

--- portfolio/src/data/challenges/javascript.ts
+++ portfolio/src/data/challenges/javascript.ts
@@ -3,6 +3,61 @@
 import type { CodingChallenge } from "./types";
 
 export const javascriptChallenges: CodingChallenge[] = [
+    // {
+    //     id: "js",
+    //     title: "",
+    //     category: "JavaScript",
+    //     approach:
+    //         "\n"+
+    //         "\n"+
+    //         "\n"+
+    //         "\n"+
+    //         "\n"+
+    //         "\n"+
+    //         "\n",
+    //     codesandboxUrl: "",
+    //     tags: [],
+    // },
+
+    {
+        id: "two-sum",
+        title: "Two Sum",
+        category: "JavaScript",
+        approach:
+            "create a two sum function that accepts array and target\n" +
+            "   create a map\n" +
+            "   loop the array\n" +
+            "       create compliment, target - array[i]\n" +
+            "       if map has compliment\n" +
+            "           return array of map get that compliment and i\n" +
+            "       map set that array[i] and i\n" +
+            "   return empty array",
+        codesandboxUrl: "https://codesandbox.io/p/devbox/8d6mv8",
+        tags: ["leetcode", "array", "hashmap"],
+    },
+    {
+        id: "valid-palindrome",
+        title: "Valid Palindrome",
+        category: "JavaScript",
+        approach:
+            "create a valid palindrome function that accepts two arrays\n" +
+            "   set left to 0\n" +
+            "   set right to s2 length - 1\n" +
+            "   while left smaller than s1.length AND right bigger than 0\n" +
+            "       if s1[left] is not the same as s2[right]\n" +
+            "           return false\n" +
+            "           left++, right--\n" +
+            "   return s1 length ==== s2 length\n",
+        codesandboxUrl: "https://codesandbox.io/p/devbox/gwrnrv",
+        tags: [
+            "leetcode",
+            "two pointer",
+            "string to array",
+            "reverse()",
+            "split()",
+            "join()",
+        ],
+    },
     {
         id: "flat",
         title: "Prototype function myFlat",
